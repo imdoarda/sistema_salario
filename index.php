3cf99ad24a0f545a031533c61ac7729e4637b826
imdoarda <dudinha.mgalvao@gmail.com>
2024-03-25 11:16:29 -0300

    teste

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Tipo número</title>
+    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+</head>
+<body>
+    <h1>Informe os dados pedidos</h1>
+    <form method="post">
+    <label for="name">NOME:</label>
+    <input type="name" name="name" />
+    <input type="submit" name="submit" value="Calcular" />
+    </form>
+    <?php 
+
+     //pedir nome vendedor
+
+
+     //valor meta semanal 20 mil reais
+
+
+     //valor meta mensal 80 mil reais
+
+
+     //calcular o salário final
+
+     //exibir ao usuário
+     //tem salario minimo definido
+     //Para o cumprimento de meta semanal: receberá  1% sobre o valor da meta.
+     //Para o excedente de meta semanal: Receberá 5% sobre o excedente da meta semanal.
+     //Para o excedente de meta mensal: Receberá 10% sobre o excedente de meta mensal.
+
+
+    ?>
+</body>

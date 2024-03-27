09646e819b0fd0055e06a99d8f227691b045fc64
imdoarda <dudinha.mgalvao@gmail.com>
2024-03-27 11:43:21 -0300

    mm

--- index.php
+++ index.php
@@ -21,85 +21,108 @@
 
     <label for="name">QUAL O VALOR FEITO NA SEMANA 2?:</label>
     <input type="number" name="feito1" />
-
+    
     <label for="name">QUAL O VALOR FEITO NA SEMANA 3?:</label>
     <input type="number" name="feito2" />
-
+    
     <label for="name">QUAL O VALOR FEITO NA SEMANA 4?:</label>
     <input type="number" name="feito3" />
-
-    <!--informa valor feito no mês-->
-    <label for="name">QUAL O VALOR FEITO NO MÊS?:</label>
-    <input type="number" name="month" />
 
     <!--botão p/adicionar as informações-->
     <button type="submit" name="submit">Calcular</button>
     </form>
-    </div>
+</div>
 
 
-    <?php 
+<?php 
 
-    //variáveis
-    $num2 = filter_input(INPUT_POST, "feito");
-    $num3 = filter_input(INPUT_POST, "feito1");
-    $num4 = filter_input(INPUT_POST, "feito2");
-    $num5 = filter_input(INPUT_POST, "feito3");
-    $num6 = filter_input(INPUT_POST, "month");
-   
+//variáveis
+$num2 = filter_input(INPUT_POST, "feito");
+$num3 = filter_input(INPUT_POST, "feito1");
+$num4 = filter_input(INPUT_POST, "feito2");
+$num5 = filter_input(INPUT_POST, "feito3");
+$num6 = intval($num2) + intval($num3) + intval($num4) + intval($num5);
 
+if(!($num2 && $num3 && $num4 && $num5)){
+    echo "Por favor, informe os dados pedidos.";
+    return;
+}
+if($num2 < 0 || $num3 < 0 || $num4 < 0 || $num5 < 0){
+    echo "Por favor, informe com dados positivos.";
+    return;
+}
 
-    //valores fixos
-   
-    $metafixaSemanal = 20000;
-    $metafixaMensal = 80000;
-    $salario_minimo = 1927.02;
+//valores fixos
+
+$metafixaSemanal = 20000;
+$metafixaMensal = 80000;
+$salario_minimo = 1927.02;
+
+//Cálculo excedente
+
+$valor1 = 0;
+$percentualsem1 = 0;
+    if ($num2 >= $metafixaSemanal) {
+        $percentualsem1 = 1.0 / 100.0 * $metafixaSemanal;
+        echo "no if (" . $percentualsem1 . ")<br>";
+        $valor1 = intval($num2) - intval($metafixaSemanal); //excedente semana 1
+    }
+    echo "fora do if (" . $valor1 . ")<br>";
     
-    //Cálculo excedente
-    
-     $valor1 = intval($num2) - intval($metafixaSemanal); //excedente semana 1
-     $valor2 = intval($num3) - intval($metafixaSemanal); //excedente semana 2
-     $valor3 = intval($num4) - intval($metafixaSemanal); //excedente semana 3
-     $valor4 = intval($num5) - intval($metafixaSemanal); //excedente semana 4
+    $valor2 = 0;
+    $percentualsem2 = 0;
+    if ($num3 >= $metafixaSemanal) {
+        $percentualsem2 = 1.0 / 100.0 * $metafixaSemanal;
+        echo "no if (" . $percentualsem2 . ")<br>";
+        $valor2 = intval($num2) - intval($metafixaSemanal); //excedente semana 2
+    }
+    echo "fora do if (" . $valor2 . ")<br>";
 
-     $valor5 = intval($num6) - intval($metafixaMensal); //excedente mês
+    $valor3 = 0;
+    $percentualsem3= 0;
+    if ($num4 >= $metafixaSemanal) {
+        $percentualsem3 = 1.0 / 100.0 * $metafixaSemanal;
+        echo "no if (" . $percentualsem3 . ")<br>";
+        $valor3 = intval($num4) - intval($metafixaSemanal); //excedente semana 3
+    }
+    echo "fora do if (" . $valor3 . ")<br>";
+     
+    $valor4 = 0;
+    $percentualsem4 = 0;
+    if ($num5 >= $metafixaSemanal) {
+        $percentualsem4 = 1.0 / 100.0 * $metafixaSemanal;
+        echo "no if (" . $percentualsem4 . ")<br>";
+        $valor4 = intval($num5) - intval($metafixaSemanal); //excedente semana 4
+    }
+    echo "fora do if (" . $valor4 . ")<br>";
 
+    $valor5 = 0;
+    if ($num6 > $metafixaMensal) {
+        $valor5 = intval($num6) - intval($metafixaMensal); //excedente mês
+    }
+    echo "fora do if (" . $valor5 . ")<br>";
+    //inicia tudo como zero e, em caso de excedente, conta o valor
     
     //calculo metas concluidas
 
-    $percentual1 = 1.0 / 100.0 * $metafixaSemanal;
-    $percentual2 = 5.0 / 100.0 * $valor1; 
-    $percentual3 = 5.0 / 100.0 * $valor2; 
-    $percentual4 = 5.0 / 100.0 * $valor3; 
-    $percentual5 = 5.0 / 100.0 * $valor4; 
+    $percentualexc2 = 5.0 / 100.0 * $valor1; 
+    $percentualexc3 = 5.0 / 100.0 * $valor2; 
+    $percentualexc4 = 5.0 / 100.0 * $valor3; 
+    $percentualexc5 = 5.0 / 100.0 * $valor4; 
 
+     
     $percentual6 = 10.0 / 100.0 * $valor5;  //$metafixaMensal;
     
 
     //Cálculo salário
 
-    $salario1 = $percentual1 + $salario_minimo;
-    $salario2 = $percentual2 + $salario_minimo;
-    $salario3 = $percentual3 + $salario_minimo;
-    $salario4 = $percentual4 + $salario_minimo;
-    $salario5 = $percentual5 + $salario_minimo;
+    $salario6 = $percentual6;
 
+    $salariofinal = $salario_minimo + $percentualsem1 + $percentualsem2 + $percentualsem3 + $percentualsem4 +
+                    $percentualexc2 + $percentualexc3 + $percentualexc4 + $percentualexc5 ;//+ $percentual6;
 
-    $salario6 = $percentual6 + $salario_minimo;
-    $salario7 = $percentual1 * 4 + $salario_minimo;
+    echo $salariofinal;
 
-    
-
-
-
-    if(!($num2 && $num3 && $num4 && $num5 && $num6)){
-        echo "Por favor, informe os dados pedidos.";
-        return;
-    }
-    if($num2 < 0 || $num3 < 0 || $num4 < 0 || $num5 < 0 || $num6 < 0){
-        echo "Por favor, informe com dados positivos.";
-        return;
-    }
 
 
     

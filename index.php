a7d75e64d850fd3309226e955db42c6f32a2e862
imdoarda <dudinha.mgalvao@gmail.com>
2024-03-27 08:48:04 -0300

    aa

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 <head>
     <title>Tipo número</title>
     <link rel='stylesheet' type='text/css' media='screen' href='_css/style.css'>
+    <script src='index.js'></script>
+
 </head>
 <body>
     <div class = "container">
@@ -11,7 +13,7 @@
     <h1>Informe os dados pedidos</h1>
     <form method="post">
     <label for="name">NOME:</label>
-    <input type="name" name="name" />
+    <input type="text" name="name" />
 
     <!--informa valor meta semanal (20 mil reais)-->
     <label for="name">VALOR DA META SEMANAL:</label>
@@ -40,7 +42,7 @@
 
     <!--botão p/adicionar as informações-->
     <button type="submit" name="submit">Calcular</button>
-    </form>]
+    </form>
     </div>
 
 
@@ -54,6 +56,7 @@
     $num4 = filter_input(INPUT_POST, "feito2");
     $num5 = filter_input(INPUT_POST, "feito3");
     $num6 = filter_input(INPUT_POST, "month");
+    //$tot = filter_input(INPUT_POST, "feito", "feito1","feito2", "feito3");
 
    
 
@@ -66,14 +69,23 @@
     
     //Cálculo excedente
     
-     $valor1 = $num2 - $num; //excedente semana
-     $valor2 = $num6 - $num1; //excedente mês
+     $valor1 = intval($num2) - intval($num); //excedente semana 1
+     $valor2 = intval($num3) - intval($num); //excedente semana 2
+     $valor3 = intval($num4) - intval($num); //excedente semana 3
+     $valor4 = intval($num5) - intval($num); //excedente semana 4
 
+     $valor5 = intval($num6) - intval($num1); //excedente mês
+
+    
     //calculo metas concluidas
 
     $percentual1 = 1.0 / 100.0 * $metafixaSemanal;
-    $percentual2 = 5.0 / 100.0 * $valor1; //$metafixaSemanal;
-    $percentual3 = 10.0 / 100.0 * $valor2;  //$metafixaMensal;
+    $percentual2 = 5.0 / 100.0 * $valor1; 
+    $percentual3 = 5.0 / 100.0 * $valor2; 
+    $percentual4 = 5.0 / 100.0 * $valor3; 
+    $percentual5 = 5.0 / 100.0 * $valor4; 
+
+    $percentual6 = 10.0 / 100.0 * $valor5;  //$metafixaMensal;
     
 
     //Cálculo salário
@@ -81,7 +93,14 @@
     $salario1 = $percentual1 + $salario_minimo;
     $salario2 = $percentual2 + $salario_minimo;
     $salario3 = $percentual3 + $salario_minimo;
-    $salario4 = $percentual1 * 4 + $salario_minimo;
+    $salario4 = $percentual4 + $salario_minimo;
+    $salario5 = $percentual5 + $salario_minimo;
+
+
+    $salario6 = $percentual6 + $salario_minimo;
+    $salario7 = $percentual1 * 4 + $salario_minimo;
+
+    
 
    
 
@@ -101,21 +120,35 @@
     if($num2 && $num3 && $num4 && $num5 == $num){
         echo "O funcionário alcançou a meta semanal, seu salário deve ser de: $salario1 .";
     }else if($num2 + $num3 + $num4 + $num5 == $metafixaMensal){
-        echo "O funcionário alcançou a meta mensal, seu salário deve ser de: $salario4";
-    }
-    
-    //condição p/ caso de obter excedente
-    if($num2 || $num3 || $num4 || $num5 > $num){
-        echo "O funcionário conseguiu alcançar a meta semanal e conseguiu excedente, seu salário deve ser de: $salario2 .";
-    } else if($num2 < $num){
-        echo "O funcionário não atingiu a meta semanal e, assim, seu salário deve ser de: $salario_minimo .";
-    } 
-
-    if($num6 == $num1){
-        echo "O funcionário alcançou a meta mensal, seu salário deve ser de: $salario4 .";
+        echo "O funcionário alcançou a meta mensal, seu salário deve ser de: $salario7";
     }else if($num6 > $num1){
         echo "O funcionário alcançou a meta mensal com excedente, seu salário deve ser de: ";
     }
+    
+    //condição p/ caso de obter excedente
+    if($num2 > $num){
+        echo "O funcionário conseguiu alcançar a meta semanal e conseguiu excedente, seu salário deve ser de: $salario2 .";
+    } else if($num3 > $num){
+        echo"O funcionário conseguiu alcançar a meta semanal e conseguiu excedente, seu salário deve ser de: $salario3 .";    
+    } else if($num4 > $num){
+        echo"O funcionário conseguiu alcançar a meta semanal e conseguiu excedente, seu salário deve ser de: $salario4 .";
+    } else if($num5 > $num){
+        echo"O funcionário conseguiu alcançar a meta semanal e conseguiu excedente, seu salário deve ser de: $salario5 .";
+    }
+    
+    
+    if($num2 < $num){
+        echo "O funcionário não atingiu a meta semanal na primeira semana e, assim, seu salário deve ser de: $salario_minimo .";
+    } else if($num3 < $num){
+        echo "O funcionário não atingiu a meta semanal e, assim, seu salário deve ser de: $salario_minimo .";
+    } else if($num4 < $num){
+        echo "O funcionário não atingiu a meta semanal e, assim, seu salário deve ser de: $salario_minimo .";
+    } else if($num5 < $num){
+        echo "O funcionário não atingiu a meta semanal e, assim, seu salário deve ser de: $salario_minimo .";
+    } 
+
+   
+    
 
     
     

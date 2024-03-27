d4eee500ff3a02d3a082fef551d1bb72e48d75e3
imdoarda <dudinha.mgalvao@gmail.com>
2024-03-27 12:13:01 -0300

    correção código

--- index.php
+++ index.php
@@ -16,16 +16,16 @@
     <input type="text" name="name" />
     
     <!--informa valor feito na semana-->
-    <label for="name">QUAL O VALOR FEITO NA SEMANA 1?:</label>
+    <label for="name">QUAL O VALOR FEITO NA SEMANA 1?</label>
     <input type="number" name="feito" />
 
-    <label for="name">QUAL O VALOR FEITO NA SEMANA 2?:</label>
+    <label for="name">QUAL O VALOR FEITO NA SEMANA 2?</label>
     <input type="number" name="feito1" />
     
-    <label for="name">QUAL O VALOR FEITO NA SEMANA 3?:</label>
+    <label for="name">QUAL O VALOR FEITO NA SEMANA 3?</label>
     <input type="number" name="feito2" />
     
-    <label for="name">QUAL O VALOR FEITO NA SEMANA 4?:</label>
+    <label for="name">QUAL O VALOR FEITO NA SEMANA 4?</label>
     <input type="number" name="feito3" />
 
     <!--botão p/adicionar as informações-->
@@ -64,43 +64,43 @@ $valor1 = 0;
 $percentualsem1 = 0;
     if ($num2 >= $metafixaSemanal) {
         $percentualsem1 = 1.0 / 100.0 * $metafixaSemanal;
-        echo "no if (" . $percentualsem1 . ")<br>";
+        //echo "no if (" . $percentualsem1 . ")<br>";
         $valor1 = intval($num2) - intval($metafixaSemanal); //excedente semana 1
     }
-    echo "fora do if (" . $valor1 . ")<br>";
+    //echo "fora do if (" . $valor1 . ")<br>";
     
     $valor2 = 0;
     $percentualsem2 = 0;
     if ($num3 >= $metafixaSemanal) {
         $percentualsem2 = 1.0 / 100.0 * $metafixaSemanal;
-        echo "no if (" . $percentualsem2 . ")<br>";
+        //echo "no if (" . $percentualsem2 . ")<br>";
         $valor2 = intval($num2) - intval($metafixaSemanal); //excedente semana 2
     }
-    echo "fora do if (" . $valor2 . ")<br>";
+    //echo "fora do if (" . $valor2 . ")<br>";
 
     $valor3 = 0;
     $percentualsem3= 0;
     if ($num4 >= $metafixaSemanal) {
         $percentualsem3 = 1.0 / 100.0 * $metafixaSemanal;
-        echo "no if (" . $percentualsem3 . ")<br>";
+        //echo "no if (" . $percentualsem3 . ")<br>";
         $valor3 = intval($num4) - intval($metafixaSemanal); //excedente semana 3
     }
-    echo "fora do if (" . $valor3 . ")<br>";
+    //echo "fora do if (" . $valor3 . ")<br>";
      
     $valor4 = 0;
     $percentualsem4 = 0;
     if ($num5 >= $metafixaSemanal) {
         $percentualsem4 = 1.0 / 100.0 * $metafixaSemanal;
-        echo "no if (" . $percentualsem4 . ")<br>";
+        //echo "no if (" . $percentualsem4 . ")<br>";
         $valor4 = intval($num5) - intval($metafixaSemanal); //excedente semana 4
     }
-    echo "fora do if (" . $valor4 . ")<br>";
+    //echo "fora do if (" . $valor4 . ")<br>";
 
     $valor5 = 0;
     if ($num6 > $metafixaMensal) {
         $valor5 = intval($num6) - intval($metafixaMensal); //excedente mês
     }
-    echo "fora do if (" . $valor5 . ")<br>";
+    //echo "fora do if (" . $valor5 . ")<br>";
     //inicia tudo como zero e, em caso de excedente, conta o valor
     
     //calculo metas concluidas
@@ -119,9 +119,9 @@ $percentualsem1 = 0;
     $salario6 = $percentual6;
 
     $salariofinal = $salario_minimo + $percentualsem1 + $percentualsem2 + $percentualsem3 + $percentualsem4 +
-                    $percentualexc2 + $percentualexc3 + $percentualexc4 + $percentualexc5 ;//+ $percentual6;
+                    $percentualexc2 + $percentualexc3 + $percentualexc4 + $percentualexc5 + $percentual6;
 
-    echo $salariofinal;
+    echo "<h2>O salário final do funcionário deve ser de: $salariofinal</h2>";
 
 
 
